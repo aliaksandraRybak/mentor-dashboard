1acc661f861db8e896dc862223c6e6cd932735d5
aliaksandraRybak <aliaksandra.rybak@gmail.com>
2019-02-22 20:04:36 +0300

    Updates

---
+++ precache-manifest.1116431eee2f90ef67f78cc3f0d2a127.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5357f7305c488abea30c",
+    "url": "/mentor-dashboard/static/js/runtime~main.5357f730.js"
+  },
+  {
+    "revision": "c5cb117d70d511eddcb7",
+    "url": "/mentor-dashboard/static/js/main.c5cb117d.chunk.js"
+  },
+  {
+    "revision": "43360447f66d8b4bd401",
+    "url": "/mentor-dashboard/static/js/2.43360447.chunk.js"
+  },
+  {
+    "revision": "c5cb117d70d511eddcb7",
+    "url": "/mentor-dashboard/static/css/main.d81e8649.chunk.css"
+  },
+  {
+    "revision": "43360447f66d8b4bd401",
+    "url": "/mentor-dashboard/static/css/2.3b84aef6.chunk.css"
+  },
+  {
+    "revision": "6c5b13e07c72ba537aa217a9243417ce",
+    "url": "/mentor-dashboard/index.html"
+  }
+];
